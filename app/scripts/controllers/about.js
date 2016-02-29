f078718a5f9c18969e7826d94ef23688a6a45129
shawn <shawn_maharaj@hotmail.com>
2016-02-28 23:12:24 -0500

    1st commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dashboardOncorpApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dashboardOncorpApp
+ */
+angular.module('dashboardOncorpApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dashboardOncorpApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dashboardOncorpApp
+ */
+angular.module('dashboardOncorpApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
